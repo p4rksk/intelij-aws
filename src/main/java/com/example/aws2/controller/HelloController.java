c71f4a89dbfb10e2077cc03dedb94140b4e9abf7
metacoding <getinthere@naver.com>
2023-09-05 12:08:33 +0900

    로그남김

--- src/main/java/com/example/aws2/controller/HelloController.java
+++ src/main/java/com/example/aws2/controller/HelloController.java
@@ -4,16 +4,21 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 public class HelloController {
     
     @GetMapping("/")
     public String home(){
+        log.info("get /");
         return "home";
     }
 
     @GetMapping("/aws/2")
     public String aws1(){
+        log.info("get /aws/2");
         return "<h1>aws2</h1>";
     }
 }
