dcda9975cda60d36e94cd8112961a7e768db775d
yeeun0118s <yeeun0118s@naver.com>
2024-03-26 16:34:01 +0900

    배포 연습

--- src/main/java/com/example/aws2/controller/HelloController.java
+++ src/main/java/com/example/aws2/controller/HelloController.java
@@ -6,13 +6,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
+@Slf4j // 이 어노테이션어을달면 log를 달 수있다
 @RestController
 public class HelloController {
     
     @GetMapping("/")
     public String home(){
+        //로그 레벨의 디폴트는 info
+        log.trace("get /");
+        log.debug("get /");
         log.info("get /");
+        log.warn("get /");
+        log.error("get /");
         return "home";
     }
 
